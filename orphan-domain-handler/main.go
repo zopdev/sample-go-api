a2e4b7e408be3406ddd734f2a727c6f9827c0cad
jatintalgotra-zd <jatin.talgotra@zop.dev>
2025-10-16 17:25:14 +0530

    Add middleware to handle all paths

--- orphan-domain-handler/main.go
+++ orphan-domain-handler/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"gofr.dev/pkg/gofr"
+	"net/http"
+	"path/filepath"
 )
 
 const defaultStaticFilePath = `./static`
@@ -10,6 +12,18 @@ func main() {
 	app := gofr.New()
 
 	staticFilePath := app.Config.GetOrDefault("STATIC_DIR_PATH", defaultStaticFilePath)
+
+	app.UseMiddleware(func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusNotFound)
+
+			filePath := filepath.Join(staticFilePath, "404.html")
+
+			http.ServeFile(w, r, filePath)
+
+			return
+		})
+	})
 
 	app.AddStaticFiles("/", staticFilePath)
 
